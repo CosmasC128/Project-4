641841a4ca255200c795ab9657cd103dd43fe4c8
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-21 12:26:50 +0100

    react app added and apps

---
+++ Busrating/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BusratingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Busrating'
---
+++ Emprating/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmpratingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Emprating'
