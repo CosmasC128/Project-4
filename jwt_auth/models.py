819457138d4fb7e570913514adadea04cc0532a3
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-21 14:47:37 +0100

    working on auth

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    profile_image = models.CharField(max_length=300)
+    first_name = models.CharField(max_length=50, default=None)
+    last_name = models.CharField(max_length=50, default=None)
+    cv = models.TextField(max_length=5000, default=None)
+    coverLetter = models.TextField(max_length=5000, default=None)
+    title = models.CharField(max_length=50, default=None)
+    description = models.TextField(max_length=5000, default=None)
+
--- project/settings.py
+++ project/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Business',
     'Busrating',
-    'Employee',
     'Emprating',
     'Jobpost',
     'jwt_auth'
@@ -132,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
