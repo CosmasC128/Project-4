2bfc7f61e04a893dccb5928732fe41e01cfad2c4
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-25 15:37:53 +0100

    for garys mental health

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -4,5 +4,3 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
