e9802ea6e823984334bbcc4f5dd410cf04a7778e
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-26 15:29:05 +0100

    all businesses-cards done

--- client/src/components/allBusinesses.js
+++ client/src/components/allBusinesses.js
@@ -1,17 +1,42 @@
-import React from 'react'
 import BusinessCard from './businessCard.js'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 const AllBusinesses = () => {
 
   // this should have a map of the businesses cards
 
-  return (
-    <>
-      <footer>
-        <p >These are all the AllBusinesses we have!!!!</p>
-        <BusinessCard />
-      </footer>
-    </>
-  )
+  const [ businesses, setBusinesses ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('api/business-profile/')
+        setBusinesses(Object.values({ ...data }))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+  // console.log( 'type of businesses: ', typeof businesses)
+  console.log(businesses, 'businesses')
+  // <Filters id="matchesFilters" handleFilterChange={handleFilterChange} handleSortBy={handleSortBy} {...filters}/>
+  // (filters.searchTerm !== '' ? searchMatches : sortedArray )
+  return (<>
+    <div className="allBusinessesWrapper">
+      <div id="aboveAllBusinessesGrid">
+        <h1 id="allBusinessesTitle">All The Businesses</h1>
+        <h2 id="allBusinessesFlavour">Search for businesses to work at, or to review.</h2>
+      </div>
+      <div className="allBusinessesGrid">
+        { businesses.map(business => { 
+          return <BusinessCard key={business.id} { ...business } />
+        })}
+      </div>
+    </div>
+  </>)
 }
 
 export default AllBusinesses
--- client/src/components/businessCard.js
+++ client/src/components/businessCard.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const BusinessCard = () => {
 
-  // this should have a link to the public business profile
+const BusinessCard = ({ id, owner, title, location, description, image }) => { //pull in usersViewed array through here as well
 
-  return (
-    <>
-      <footer>
-        <p >These are all the BusinessCard we have!!!!</p>
-      </footer>
-    </>
-  )
+  if (id){
+    console.log(id, 'id', owner, 'owner', title, 'title', location, 'location', description, 'description', image, 'image', 'FROM THE BUSINESS CARD')
+  }
+
+  return (<>
+    <div className="businessCardWrapper">
+      <Link to={`/all-businesses/${ id }`} id="businessLink">
+        <div id="businessCardTitle">{title} title</div>
+        <img className="businessCardImage" src={image} alt="Business thumbnail"></img>
+        <div className="businessCardData">
+          location {location} <br/>
+          description {description}
+        </div>  
+      </Link>  
+    </div>
+  </>)
 }
-
 export default BusinessCard
