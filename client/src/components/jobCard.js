219e82a070b184419df68161b2880b9851b50c7a
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-27 19:16:56 +0100

    application functionality

--- client/src/components/jobCard.js
+++ client/src/components/jobCard.js
@@ -7,29 +7,46 @@ const JobCard = (props) => { //pull in usersViewed array through here as well
   // console.log(props, 'props')
   // Get job post fields from passed props
   const id = props.id
-  const owner = props.owner
+  const ownerID = props.owner.id
   const title = props.title
   const location = props.location
   const businessID = props.business
+  // const available = props.available
   const text = props.text
-  const jobrole = props.jobrole.jobrole
+  const jobroleName = props.jobrole.jobrole
+  const jobroleID = props.jobrole.id
   const image = props.image
-  const Userprofiles = props.Userprofiles
+  const Userprofiles = [ ...props.Userprofiles ]
 
-  // const userID = getPayload().sub
-
+  // *** GET THE LOGGED IN USERS ID, USE IT TO FIND THEIR PROFILE
+  // that profile id will be later pushed into the applicants array in the job post
+  const userID = 4 // getPayload().sub only add this in once I have a login functionality
+  let profileID = 0
   // console.log(userID, 'userid if it exists')
 
-  // const findUserprofileId = () => {
-  //   for (let i = 0; i < allUserData.length; i++){
-  //     if (allUserData[i].owner.id === userID){
-  //       PageID = allUserData[i].id
-  //     }
-  //   }
-  // }
+  const findProfileId = () => {
+    for (let i = 0; i < usersArray.length; i++){
+      if (usersArray[i].owner.id === userID){
+        profileID = usersArray[i].id
+      }
+    }
+  }
 
-  // findMatchingId()
+  const [ usersArray, setUsersArray ] = useState([])
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('api/user-profile/')
+        setUsersArray(Object.values({ ...data }))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+  findProfileId()
+  console.log(profileID)
 
   // get the associated business to provide link to business and business name
   const [ business, setBusiness ] = useState([])
@@ -47,25 +64,30 @@ const JobCard = (props) => { //pull in usersViewed array through here as well
 
   // buttons for expanding the job post and applying
 
+  const applyButton = document.getElementById(`applyToJobCard${id}`)
+
   const handleApply = async () => {
     try {
-      console.log('youve applied to this job')
-      Userprofiles.push(4)
-      console.log(Userprofiles, 'updated profiles after push')
+      console.log(profileID, 'has applied to this job')
+      Userprofiles.push(profileID)
+      // console.log(Userprofiles, 'updated profiles after push')
       await axios.put(`api/jobposts/${id}/`, { 
-        Userprofiles: Userprofiles,
-        id: id,
-        owner: owner,
+        owner: ownerID,
+        jobrole: jobroleID,
         title: title,
         location: location,
-        businessID: businessID,
         text: text,
-        jobrole: jobrole,
+        business: businessID,
+        Userprofiles: Userprofiles,
       })
+      applyButton.style.color = 'green'
+      applyButton.disabled = 'true'
     } catch (err) {
       console.log(err)  
     }
   }
+
+
 
   const jobCard = document.getElementById(`job${id}`)
   const jobCardMax = document.getElementById(`jobCardMax${id}`)
@@ -89,14 +111,14 @@ const JobCard = (props) => { //pull in usersViewed array through here as well
       <img className="jobCardImage" src={image} alt="Job Image"></img>
       <div id="jobCardTitle">Title: {title}</div>
       <div className="jobCardLocRole">
-        location {location} jobrole {jobrole}
+        location {location} jobrole {jobroleName}
       </div>
       <div className="jobCardMin">
-        <Link to={`/all-businesses/${businessID}`} id="jobCardBusinessLink">{business.title}</Link> <br/>
+        Posted By <Link to={`/all-businesses/${businessID}`} id="jobCardBusinessLink">{business.title}</Link> <br/>
       </div>
       <div id={`jobCardMax${id}`}>
         text: {text}
-        <button id="applyToJobCard" onClick={handleApply}>Apply</button>
+        <button id={`applyToJobCard${id}`} onClick={handleApply}>Apply</button>
       </div>
     </div>
   </>)
