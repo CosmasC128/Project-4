eaa7cb46a7c0bba5dbf58463b931efa731ad71e4
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-29 06:25:45 +0100

    header changes

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,67 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useHistory, useLocation } from 'react-router-dom'
-import { userIsAuthenticated } from '../helpers/helpers'
-// import { getPayload } from '../helpers/auth.js'
-
+import { userIsAuthenticated, getTokenFromLocalStorage } from '../helpers/helpers'
+import { getPayload } from '../helpers/auth.js'
+import axios from 'axios'
 
 const Header = () => {
 
+
   const history = useHistory()
   const location = useLocation()
+  
+
+  const [ allBizData, setAllBizData ] = useState([])
+  const [ bizInfo, setBizInfo ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/business-profile/')
+        setAllBizData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+    
+  }, [])
+  let pageID = ''
+
+  const findMatchingId = () => {
+    for (let i = 0; i < allBizData.length; i++){
+      if (allBizData[i].owner.id === userID){
+        pageID = allBizData[i].id
+      }
+    }
+  }
+
+  let userID = 0
+
+
+  
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(`/api/business-profile/${pageID}`,
+          {
+            headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+          })
+        setBizInfo({ ...data })
+      } catch (error) {
+        console.log(error)
+      }
+    }    
+    getData()
+  }, [userID])
+  
+  if (userIsAuthenticated()){
+    userID = getPayload().sub
+    findMatchingId()
+  }
+
+  const ownerStuff = { ...bizInfo.owner }
+  const ownerUsername = ownerStuff.username
 
   useEffect(() => {
   }, [location.pathname])
@@ -17,8 +71,6 @@ const Header = () => {
     history.push('/')
   }
 
-  // sexy header for the whole website, don't display on MainPaige(?)
-  // Business profile is standin for private user profile that switches for whomever the user is
   return (
     <div id='navbar'>
       <div id="navwrap">
@@ -28,7 +80,7 @@ const Header = () => {
         {
           userIsAuthenticated() ? 
             <>
-              <Link to="/profile/redirector" id="navLink">Profile</Link>
+              <Link to="/profile/redirector" id="navLink">Profile: {ownerUsername}</Link>
               <span id="navLinkLogout" onClick={handleLogout}>Logout</span>
             </>
             :
